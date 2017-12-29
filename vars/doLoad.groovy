6d1a53d8f1357a03e6e86f9f3e75ff0f8cce8058
c5403 <james.p.mcshane@healthpartners.com>
2017-12-29 11:16:40 -0600

    Set classloader

--- vars/doLoad.groovy
+++ vars/doLoad.groovy
@@ -4,5 +4,6 @@ def call() {
   ClassLoader classLoader = new groovy.lang.GroovyClassLoader()
   Map[] grapez = [[group : 'org.apache.commons', module : 'commons-math3', version : '3.4.1']]
   Grape.grab(classLoader: classLoader, grapez)
+  Thread.currentThread().setContextClassLoader(classLoader)
   echo "Done grabbing"
 }
