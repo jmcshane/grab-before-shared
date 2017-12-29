597c04a2ae2d892f8904171cc00d2276246c7c63
c5403 <james.p.mcshane@healthpartners.com>
2017-12-29 11:04:36 -0600

    Groovy class loader

--- vars/load.groovy
+++ vars/load.groovy
@@ -1,7 +1,7 @@
 import groovy.grape.Grape
 
 def call() {
-  ClassLoader classLoader = ClassLoader.platformClassLoader
+  ClassLoader classLoader = new groovy.lang.GroovyClassLoader() 
   Map[] grapez = [[group : 'org.apache.commons', module : 'commons-math3', version : '3.4.1']]
   Grape.grab(classLoader: classLoader, grapez)
 }
